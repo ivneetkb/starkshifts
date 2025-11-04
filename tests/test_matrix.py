bcc6f8f5f354da01bb07dfc2bd34afec6d0c9f0e
ivneetkb <zcapikb@ucl.ac.uk>
2025-11-04 11:01:55 +0000

    Edited abstract matrix class test

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -2,7 +2,10 @@ import pytest
 from starkshifts.matrix import Matrix
 
 def test_abstract_matrix_fail():
+    """
+    Test to check for abstract method requirement.
+    """
     with pytest.raises(TypeError):
         class IncompleteMatrix(Matrix):
             pass
-        IncompleteMatrix()
+        matrix = IncompleteMatrix()
